dbfe688561a8e3c81c189ae9d71bc70f108296c3
tue8 <localtbowm@gmail.com>
2024-10-27 13:06:03 +0700

    Fixed current directory issues in mopm

--- src/mopm.c
+++ src/mopm.c
@@ -14,20 +14,18 @@
 #include "m/m_cmd.h"
 #include "m/m_debug.h"
 
-#define MOPM_VERSION "0.2.4"
+#define MOPM_VERSION "0.3.0"
 
 const char *mopm_help = "mopm Version "MOPM_VERSION"\n"
                         "\nUsage: mopm <command> <argument>\n"
-                        "To see the usage of a specific command, use: mopm <command>\n"
-                        "\nAvailable commands: help\n"
-                        "                    install\n"
+                        "To see the usage of a specific command, use: mopm <command>\n\n"
+                        "Available commands: install\n"
                         "                    uninstall\n";
 
 int main(int argc, char *argv[])
 {
   int i;
   char *cmd;
-  char *cmd_full;
   struct cmd_info info;
 
   if (argc == 1)
@@ -46,16 +44,16 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Unknown command, see 'mopm' for more details.\n");
     return 1;
   }
-  cmd = get_current_directory();
-  asprintf(&cmd_full, "%s\\%s", cmd, info.bin);
+
+  cmd = m_strdup(info.bin);
   for (i = 2; i < argc; i++)
   {
     char *str;
-    asprintf(&str, "%s %s", cmd_full, argv[i]);
-    m_free(cmd_full);
-    cmd_full = str;
+    asprintf(&str, "%s %s", cmd, argv[i]);
+    m_free(cmd);
+    cmd = str;
   }
-  system(cmd_full);
-  m_free(cmd_full);
+  system(cmd);
+  m_free(cmd);
   return 0;
 }
